670b60241a2cab548f817ee7f38ae09392bc447d
Guiwi <guillaumemariette456@yahoo.fr>
2020-05-12 15:32:16 +0200

    Update Memory.js

--- Memory.js
+++ Memory.js
@@ -31,19 +31,19 @@ function preload(){
 	
 	this.load.image('backcard','assets/cardBack_blue5.png');
 	
-	this.load.image('carteAP','assets/cardHeartsA.png');
-	this.load.image('carte2P','assets/cardHearts2.png');
-	this.load.image('carte5P','assets/cardHearts5.png');
-	this.load.image('carte7P','assets/cardHearts7.png');
-	this.load.image('carte10P','assets/cardHearts10.png');
-	this.load.image('carteQP','assets/cardHeartsQ.png');
+	this.load.image('carteAC','assets/cardHeartsA.png');
+	this.load.image('carte2C','assets/cardHearts2.png');
+	this.load.image('carte5C','assets/cardHearts5.png');
+	this.load.image('carte7C','assets/cardHearts7.png');
+	this.load.image('carte10C','assets/cardHearts10.png');
+	this.load.image('carteQC','assets/cardHeartsQ.png');
 	
-	this.load.image('carteAC','assets/cardSpadesA.png');
-	this.load.image('carte2C','assets/cardSpades2.png');
-	this.load.image('carte5C','assets/cardSpades5.png');
-	this.load.image('carte7C','assets/cardSpades7.png');
-	this.load.image('carte10C','assets/cardSpades10.png');
-	this.load.image('carteQC','assets/cardSpadesQ.png');
+	this.load.image('carteAP','assets/cardSpadesA.png');
+	this.load.image('carte2P','assets/cardSpades2.png');
+	this.load.image('carte5P','assets/cardSpades5.png');
+	this.load.image('carte7P','assets/cardSpades7.png');
+	this.load.image('carte10P','assets/cardSpades10.png');
+	this.load.image('carteQP','assets/cardSpadesQ.png');
 	
 	
 	//ICI charger les images des cartes
