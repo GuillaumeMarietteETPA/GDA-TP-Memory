26bf65cd6e518141f78237801cef4f55caa3cd2a
Guiwi <guillaumemariette456@yahoo.fr>
2020-05-12 15:29:35 +0200

    Load

--- Memory.js
+++ Memory.js
@@ -31,11 +31,32 @@ function preload(){
 	
 	this.load.image('backcard','assets/cardBack_blue5.png');
 	
+	this.load.image('carteAP','assets/cardHeartsA.png');
+	this.load.image('carte2P','assets/cardHearts2.png');
+	this.load.image('carte5P','assets/cardHearts5.png');
+	this.load.image('carte7P','assets/cardHearts7.png');
+	this.load.image('carte10P','assets/cardHearts10.png');
+	this.load.image('carteQP','assets/cardHeartsQ.png');
+	
+	this.load.image('carteAC','assets/cardSpadesA.png');
+	this.load.image('carte2C','assets/cardSpades2.png');
+	this.load.image('carte5C','assets/cardSpades5.png');
+	this.load.image('carte7C','assets/cardSpades7.png');
+	this.load.image('carte10C','assets/cardSpades10.png');
+	this.load.image('carteQC','assets/cardSpadesQ.png');
+	
+	
 	//ICI charger les images des cartes
 }
 
 
 function create(){
+	
+	
+	
+	
+	
+	
 	
 	//Créer cartes
 	//ajouter fonction random pour les mélanger
