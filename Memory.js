167c313077d3de53c03788d702a362075432cbd8
Guiwi <guillaumemariette456@yahoo.fr>
2020-05-12 14:56:57 +0200

    Update Memory.js

---
+++ Memory.js
@@ -0,0 +1,44 @@
+var config = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+
+
+var game = new Phaser.Game(config);
+
+	var carte 1;
+	var carte 2;
+
+
+
+
+function preload(){
+	
+	
+	
+}
+
+
+function create(){
+	
+	
+	
+}
+
+function update(){
+	
+	
+}
